bd76c4a177c3d0b06de6f60d724aaf3eb27d6ff5
Nathan Shelly <nsnshelly@gmail.com>
2020-06-03 00:14:52 -0500

    refactor: remove unused config

--- neovim/alias.zsh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env zsh
-
-# `enc` for `editor no config`, `-u <config file>`
-# would need to update with new editors
-alias enc="$EDITOR -u /dev/null"
-
-# `emp` for `editor markdown preview`, `-c MarkdownPreview`
-# requires https://github.com/iamcco/markdown-preview.nvim
-alias emp="$EDITOR -c MarkdownPreview"
--- tmux/alias.zsh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env zsh
-
-# launch w/o sourcing configs (`-f file` specifies config files)
-alias tmuxn='tmux -f /dev/null'
-
-alias tk='tmux kill-session -t'
-alias tl='tmux ls'
